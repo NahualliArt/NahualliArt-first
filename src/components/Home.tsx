1644bca87f1584d8d0721d7a629ab5408f54be9f
NahualliArt <artnahualli@gmail.com>
2023-02-01 19:26:51 -0600

    Home content plain

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,6 +18,7 @@ function Home() {
   
   return (
     <>
+        {/* INTRO */}
         <div>
             <div></div>
             <div>
@@ -26,6 +27,81 @@ function Home() {
                 <button>{t("home.button-intro")}</button>
             </div>
         </div>
+        {/* WELCOME */}
+        <div>
+            <div>
+                <h2>{t("home.welcome")}</h2>
+                <p>{t("home.intro")}</p>
+            </div>
+            <div></div>
+        </div>
+        {/* TEXT DECORATION */}
+        <div>
+            <h2>UI Design</h2>
+            <h2>Web Design</h2>
+            <h2>Illustration</h2>
+            <h2>Graphic Design</h2>
+        </div>
+        {/* SERVICES */}
+        <div>
+            <div>
+                <div>
+                    <h4>{t("home.work.ui-design")}</h4>
+                    <div></div>
+                    <p>{t("home.work.ui-text")}</p>
+                </div>
+                <div>
+                    <h4>{t("home.work.web-design")}</h4>
+                    <div></div>
+                    <p>{t("home.work.web-text")}</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h4>{t("home.work.illustration")}</h4>
+                    <div></div>
+                    <p>{t("home.work.illustration-text")}</p>
+                </div>
+                <div>
+                    <h4>{t("home.work.graphic-design")}</h4>
+                    <div></div>
+                    <p>{t("home.work.graphic-text")}</p>
+                </div>
+            </div>
+        </div>
+        {/* TEAM */}
+        <div>
+            <div>
+                <div>
+                    <div></div>
+                    <h3>Abigail P</h3>
+                    <p>{t("home.team.sandra-text")}</p>
+                </div>
+                <div>
+                    <div></div>
+                    <h3>Kitzia P</h3>
+                    <p>{t("home.team.kitzia-text")}</p>
+                </div>
+            </div>
+
+            <div>
+                <h3>{t("home.team.title")}</h3>
+                <p>{t("home.team.text")}</p>
+            </div>
+        </div>
+
+        {/* SLIDER IT'S A COMPONENT */}
+
+        {/* CTA */}
+        <div>
+            <div>
+                <h3>{t("home.cta.title")}</h3>
+                <p>{t("home.cta.text")}</p>
+                <p>contacto@nahualliart.com</p>
+            </div>
+            <div></div>
+        </div>
+
     </>
   );
 };
