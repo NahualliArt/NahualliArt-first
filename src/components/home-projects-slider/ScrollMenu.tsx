223af0df1e1153f043e96fb3a1c6cc94a699a016
7Kiin <78456393+7Kiin@users.noreply.github.com>
2023-02-21 15:12:53 -0600

    Nuevo array
    Array para guardar los proyectos de los sliders

--- src/components/home-projects-slider/ScrollMenu.tsx
+++ src/components/home-projects-slider/ScrollMenu.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Slider from 'react-slick';
 import item1 from './MagItem'; //este es un componente donde ira el diseño de un projecto, busca como llamarlo dentro de un array.
 
+//Se supone en esto el FC significa "function component". Se supone que esto es el arrray
+const items: React.FC[]=[item1];
+
+
+
 interface MenuItem {
   text: string;
 }
