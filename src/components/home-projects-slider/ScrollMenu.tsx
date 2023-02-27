861aa858bf356a9d5ca76600fb526d36fd5a84f8
7Kiin <78456393+7Kiin@users.noreply.github.com>
2023-02-27 17:13:11 -0600

    Pruebas que no sirven

--- src/components/home-projects-slider/ScrollMenu.tsx
+++ src/components/home-projects-slider/ScrollMenu.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-//import ItemsMap from 'react-horizontal-scrolling-menu/dist/types/ItemsMap';
-import Slider from 'react-slick';
+//import Slider from 'react-slick';
 import Item1 from './MagItem'; //este es un componente donde ira el diseño de un projecto, busca como llamarlo dentro de un array.
 import Item2 from './BizItem';
+
 
 //Se supone en esto el FC significa "function component". Se supone que esto es el arrray
 const projects: React.FC[]=[Item1, Item2];
 
 
 //Aquí se supone que se hace render, pero todavía no sé bien qué significa eso 
-const App: React.FC = () => {
+ const App: React.FC = () => {
   return (
     <div>
       {projects.map((Project, index) => (
@@ -18,7 +18,7 @@ const App: React.FC = () => {
     </div>
   );
 };
-
+/*No sirve
 interface MenuItem {
   text: string;
 }
@@ -26,9 +26,7 @@ interface MenuItem {
 
 
 interface ScrollMenuProps {
-  items: MenuItem[
-    
-  ];
+  items: MenuItem[];
 }
 
 const ScrollMenu: React.FC<ScrollMenuProps> = ({ items }) => {
@@ -53,7 +51,7 @@ const ScrollMenu: React.FC<ScrollMenuProps> = ({ items }) => {
   );
 };
 
-export default ScrollMenu;
+export default ScrollMenu; */
 
 
 
