f0e3c65df001c67e8d14e7434c3aec093138dcd4
7Kiin <78456393+7Kiin@users.noreply.github.com>
2023-03-02 16:36:41 -0600

    Borre todo alv

--- src/components/home-projects-slider/ScrollMenu.tsx
+++ src/components/home-projects-slider/ScrollMenu.tsx
@@ -22,41 +22,6 @@ const projects: React.FC[]=[Item1, Item2];
 };
 
 
-/*No sirve
-interface MenuItem {
-  text: string;
-}
-
-
-
-interface ScrollMenuProps {
-  items: MenuItem[];
-}
-
-const ScrollMenu: React.FC<ScrollMenuProps> = ({ items }) => {
-  const menuItems = items.map((item) => (
-    <div key={item.text}>{item.text}</div>
-  ));
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  };
-
-  return (
-    <div>
-      <Slider {...settings}>
-        {menuItems}
-      </Slider>
-    </div>
-  );
-};
-
-export default ScrollMenu; */
-
 
 
 
