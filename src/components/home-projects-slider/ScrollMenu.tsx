10fd448f0f1cef600670e8059abb59f5dfefc2c7
7Kiin <78456393+7Kiin@users.noreply.github.com>
2023-03-02 13:40:06 -0600

    pruebas slider

--- src/components/home-projects-slider/ScrollMenu.tsx
+++ src/components/home-projects-slider/ScrollMenu.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-//import Slider from 'react-slick';
+import Slider from 'react-slick';
 import Item1 from './MagItem'; //este es un componente donde ira el diseño de un projecto, busca como llamarlo dentro de un array.
 import Item2 from './BizItem';
 
+type SliderProps = {
+  slides: React.ReactNode[];
+};
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const SliderComponent: React.FC<SliderProps> = ({ slides }) => {
+  return (
+    <Slider {...settings}>
+      {slides.map((slide, index) => (
+        <div key={index}>{slide}</div>
+      ))}
+    </Slider>
+  );
+};
+
+export default SliderComponent;
 
 //Se supone en esto el FC significa "function component". Se supone que esto es el arrray
 const projects: React.FC[]=[Item1, Item2];
@@ -10,8 +33,16 @@ const projects: React.FC[]=[Item1, Item2];
 
 //Aquí se supone que se hace render, pero todavía no sé bien qué significa eso 
  const App: React.FC = () => {
+
+  const slides = [
+    <div key="1">Slide 1</div>,
+    <div key="2">Slide 2</div>,
+    <div key="3">Slide 3</div>,
+  ];
+
   return (
     <div>
+      <Slider slides ={slides}/>
       {projects.map((Project, index) => (
         <Project key={index} />
       ))}
@@ -19,19 +50,7 @@ const projects: React.FC[]=[Item1, Item2];
   );
 };
 
-class ScrollHandler{
-  private duration:number;
-	private scrolling:boolean;
-	private pages:number;
 
-  constructor(containerId:string,duration:number=500) {
-    this.pages = projects.map;
-    this.scrolling = false;
-		this.duration = duration;
-		this.hook();
-  }
-
-}
 /*No sirve
 interface MenuItem {
   text: string;
