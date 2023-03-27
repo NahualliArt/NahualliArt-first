74447f54aa9c38174015cd474db8ffe22a7cce58
7Kiin <78456393+7Kiin@users.noreply.github.com>
2023-03-27 13:19:44 -0600

    aaaa

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NahualliArt/NahualliArt/',
+  base: '/NahualliArt/',
   plugins: [react()],
   
 });
